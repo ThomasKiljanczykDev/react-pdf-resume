9b711a53c2435529a3d8f85e55bbe6416843a7fc
thomas kiljanczyk dev <thomas.kiljanczyk.dev@gmail.com>
2026-01-02 17:46:08 +0100

    chore: update dependencies

--- eslint/typescript.js
+++ eslint/typescript.js
@@ -1,34 +1,28 @@
 // @ts-check
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-    ...tseslint.configs.recommended.map(config => ({
-        ...config,
-        files: [...(config.files ?? []), '**/*.ts', '**/*.tsx']
-    })),
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        plugins: {
-            '@typescript-eslint': tseslint.plugin
-        },
-        languageOptions: {
-            parser: tseslint.parser,
-            parserOptions: {
-                projectService: true,
-                ecmaVersion: 'latest',
-                sourceType: 'module'
-            }
-        },
-        rules: {
-            '@typescript-eslint/no-shadow': 'error',
-            '@typescript-eslint/no-restricted-imports': [
-                'error',
-                {
-                    name: 'react-redux',
-                    importNames: ['useSelector', 'useDispatch', 'createSlice'],
-                    message: 'Use typed hooks `useAppDispatch` and `useAppSelector` instead.'
-                }
-            ]
+export default tseslint.config(...tseslint.configs.recommended, {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+        '@typescript-eslint': tseslint.plugin
+    },
+    languageOptions: {
+        parser: tseslint.parser,
+        parserOptions: {
+            projectService: true,
+            ecmaVersion: 'latest',
+            sourceType: 'module'
         }
+    },
+    rules: {
+        '@typescript-eslint/no-shadow': 'error',
+        '@typescript-eslint/no-restricted-imports': [
+            'error',
+            {
+                name: 'react-redux',
+                importNames: ['useSelector', 'useDispatch', 'createSlice'],
+                message: 'Use typed hooks `useAppDispatch` and `useAppSelector` instead.'
+            }
+        ]
     }
-);
+});
